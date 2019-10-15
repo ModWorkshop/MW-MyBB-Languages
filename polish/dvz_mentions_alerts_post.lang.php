9f3b52efe97e7d79d14a7de957f449507e1680c9
Croveaux <56558427+Croveaux@users.noreply.github.com>
2019-10-15 16:33:05 +0200

    Update dvz_mentions_alerts_post.lang.php

--- polish/dvz_mentions_alerts_post.lang.php
+++ polish/dvz_mentions_alerts_post.lang.php
@@ -2,5 +2,5 @@
 /* by Tomasz 'Devilshakerz' Mlynski [devilshakerz.com]; Copyright (C) 2014-2016
  released under Creative Commons BY-NC-SA 4.0 license: http://creativecommons.org/licenses/by-nc-sa/4.0/ */
 
-$l['myalerts_dvz_mentions_post_alert'] = '{1} mentioned you in "{2}".';
-$l['myalerts_setting_dvz_mentions_post'] = 'Receive alert when mentioned in a post?';
+$l['myalerts_dvz_mentions_post_alert'] = '{1} wspomniał o tobie w "{2}".';
+$l['myalerts_setting_dvz_mentions_post'] = 'Czy chcesz dostawać alerty w razie bycia wspomnianym w postach?';
