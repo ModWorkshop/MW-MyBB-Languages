680b476b286eadf1b4a92ff34edb5813a2d14190
Croveaux <56558427+Croveaux@users.noreply.github.com>
2019-10-18 16:38:56 +0200

    Update mention.lang.php

--- polish/mention.lang.php
+++ polish/mention.lang.php
@@ -7,24 +7,24 @@
  * this file contains language definitions for MentionMe (English)
  */
 
-$l['mention'] = 'mention';
-$l['mention_link_title'] = "{1}'s profile";
+$l['mention'] = 'wspomnij';
+$l['mention_link_title'] = "profil użytkownika {1}";
 
 // task
 $l['mention_task_success'] = 'The MentionMe name cache task ran successfully. Going back {1} days, {2} users were stored at a total cache size of {3}';
 $l['mention_task_fail'] = 'MentionMe found no users to cache information for.';
 
 // postbit button
-$l['mention_button'] = 'Mention';
-$l['mention_title'] = 'Mention user';
-$l['mention_posts_selected'] = 'You have selected one or more users to mention.';
-$l['mention_users_now'] = 'Mention these users now';
+$l['mention_button'] = 'Wspomnienia';
+$l['mention_title'] = 'Wspomnij o użytkowniku';
+$l['mention_posts_selected'] = 'Wybrałeś więcej niż jednego użytkownika do wspomnienia.';
+$l['mention_users_now'] = 'Wspomnij o tych użytkownikach teraz.';
 
 // MyAlerts
-$l['myalerts_mention_alert'] = '{1} mentioned you in "<strong>{2}</strong>".';
-$l['myalerts_setting_mention'] = 'Receive alert when mentioned in a post?';
+$l['myalerts_mention_alert'] = '{1} wspomniał o tobie w "<strong>{2}</strong>".';
+$l['myalerts_setting_mention'] = 'Czy chcesz dostawać powiadomienia, gdy zostaniesz wspomniany w poście?';
 
 // mention autocomplete
-$l['mention_autocomplete_instructions'] = 'type a user name';
+$l['mention_autocomplete_instructions'] = 'wpisz imię użytkownika';
 
 ?>
